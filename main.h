58f2d11eed8a93148691f70f76298d57b5648bf5
jihane <jihanebhsbhs@gmail.com>
2023-05-28 15:49:20 -0700

    main.h file update

--- main.h
+++ main.h
@@ -1,6 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-int _printf(const char *format, ...);
-
-#endif
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+#define S_LONG 2
+#define S_SHORT 1
+struct fmt
+{
+	char fmt;
+	int(*fn)(va_list,char[],int,int,int,int);
+};
+typedef struct fmt fmt_t;
+int _printf(const char *format,...);
+int handle_print(const char
