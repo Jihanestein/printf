389e72b265088433b4c1629771b0b322eed343ca
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-06-01 17:58:16 -0700

    myfunctions01.c

--- myfunctions01.c
+++ myfunctions01.c
@@ -1,5 +1,39 @@
 #include "myfunctions0.h"
 #include <stddef.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes a caharacter to the standard output
+ * Return: 1 upon successful completion
+ * @c: The character to be written
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+#include "myfunctions0.h"
+#include <unistd.h>
+
+/**
+ * _puts - writes a string to the standard output
+ *@str: the string to be written
+ * Return: The number of characters written
+ */
+int _puts(const char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		if (_putchar(str) < 0)
+			return (-1);
+		count++;
+		str++;
+	}
+
+	return (count);
+}
 
 /**
  * handl_buf - A funcntion that handles a buffer
@@ -26,4 +60,3 @@ int ev_print_func(const char *format, int index)
 	(void)index;
 	return (0);
 }
-
