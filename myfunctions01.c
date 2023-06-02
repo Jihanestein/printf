2d7b30bfacc2dd523b7f2ce907a95efba979a495
Genevieve C Onwuka <genevievechigoziri@gmail.com>
2023-06-02 10:52:16 -0700

    myfunctions01.c

--- myfunctions01.c
+++ myfunctions01.c
@@ -33,30 +33,3 @@ int _puts(const char *str)
 	}
 
 	return (count);
-}
-
-/**
- * handl_buf - A funcntion that handles a buffer
- * @buffer: The buffer to be handled
- * @format: The format specifier
- * @size: The size of the buffer
- */
-void handl_buf(char *buffer, char format, unsigned int size)
-{
-	(void)buffer;
-	(void)format;
-	(void)size;
-}
-
-/**
- * ev_print_func - Event print function
- * @format: The format specifier
- * @index: The index
- * Return: Always 0 on success
- */
-int ev_print_func(const char *format, int index)
-{
-	(void)format;
-	(void)index;
-	return (0);
-}
